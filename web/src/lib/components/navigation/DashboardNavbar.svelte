806e37ab9121365e22a617508fee0565eaa808db
Clarkden <63682937+Clarkden@users.noreply.github.com>
2023-07-26 11:17:59 -0700

    Mobile nav bar

--- web/src/lib/components/navigation/DashboardNavbar.svelte
+++ web/src/lib/components/navigation/DashboardNavbar.svelte
@@ -4,10 +4,11 @@
   import profile from "$lib/assets/images/standardUserImage.svg";
   import { createDropdownMenu } from "@melt-ui/svelte";
   import { writable } from "svelte/store";
+  import { slide } from "svelte/transition";
 
   const { trigger, menu, item, separator } = createDropdownMenu();
-
   const hideMenu = writable(false);
+  let hideMobileNav = true;
 </script>
 
 <div
@@ -55,4 +56,40 @@
             <li><button on:click={logout}>Logout</button></li>
         </ul> -->
   </ul>
+  <button class="md:hidden" on:click={() => (hideMobileNav = !hideMobileNav)}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 text-black"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      {#if hideMobileNav}
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16m-7 6h7"
+        />
+      {:else}
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      {/if}
+    </svg>
+  </button>
+  {#if !hideMobileNav}
+    <ul
+      class="flex md:hidden flex-col gap-3 absolute top-14 left-0 right-0 p-5 bg-indigo-400 text-white font-semibold border-b-black border-b"
+      in:slide
+      out:slide
+    >
+      <li><a href="/dashboard/profile">Profile</a></li>
+      <li><a href="/dashboard/settings">Settings</a></li>
+      <li><button on:click={logout}>Logout</button></li>
+    </ul>
+  {/if}
 </div>
--- web/src/lib/components/navigation/Navbar.svelte
+++ web/src/lib/components/navigation/Navbar.svelte
@@ -1,15 +1,55 @@
 <script lang="ts">
   import { page } from "$app/stores";
+  import { slide } from "svelte/transition";
+
+  let hideMobileNav = true;
 </script>
 
 <div
-  class="flex flex-row justify-between items-center px-5 w-full h-14 top-0 border-b border-black"
+  class="flex flex-row justify-between items-center px-5 w-full h-14 top-0 border-b border-black relative"
 >
-  <a href="/"><h1>Discord Pay</h1></a>
+  <a href="/"><h1 class="font-bold">Discord Pay</h1></a>
   <ul class="hidden md:flex flex-row gap-3">
     {#if $page.data.user}
       <a href="/dashboard">Dashboard</a>
     {:else if !$page.url.pathname.includes("/login")}
       <a href="/login">Login</a>{/if}
   </ul>
+  <button class="md:hidden" on:click={() => (hideMobileNav = !hideMobileNav)}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 text-black"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      {#if hideMobileNav}
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16m-7 6h7"
+        />
+      {:else}
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      {/if}
+    </svg>
+  </button>
+  {#if !hideMobileNav}
+    <ul
+      class="flex md:hidden flex-col gap-3 absolute top-14 left-0 right-0 p-5 bg-indigo-400 text-white font-semibold border-b-black border-b"
+      in:slide
+      out:slide
+    >
+      {#if $page.data.user}
+        <li><a href="/dashboard">Dashboard</a></li>
+      {:else if !$page.url.pathname.includes("/login")}
+        <li><a href="/login">Login</a></li>{/if}
+    </ul>
+  {/if}
 </div>
